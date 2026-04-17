aafaf090d7e6914fe40fca6fe1e019dedba3cdc2
Rakshitha S <rakshi69292@gmail.com>
2026-04-17 19:32:18 +0530

    Change app.run to use host and port parameters

--- app.py
+++ app.py
@@ -32,5 +32,7 @@ def withdraw():
 def show_transactions():
     return render_template('transactions.html', transactions=transactions)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
