c44a2a473182b466b4b2610132ea6aaea0ca0f74
Rakshitha S <rakshi69292@gmail.com>
2026-04-17 18:46:46 +0530

    Refactor bank system to use Flask web framework

--- app.py
+++ app.py
@@ -1,103 +1,36 @@
-class Customer:
-    def __init__(self):
-        self.id = ""
-        self.name = ""
+from flask import Flask, render_template, request
 
-    def registration(self, id, name):
-        self.id = id
-        self.name = name
-        print("Registration successful")
-
-    def display(self):
-        print("Customer ID:", self.id)
-        print("Name:", self.name)
-
-
-class Account:
-    def __init__(self, acc_no=1, balance=100):
-        self.acc_no = acc_no
-        self.balance = balance
-
-    def deposit(self, amount):
-        self.balance += amount
-        print("Deposited:", amount)
-        print("-------------------------")
-
-    def withdrawal(self, amount):
-        if amount > self.balance:
-            print("Insufficient balance")
-            print("-------------------------")
-        else:
-            self.balance -= amount
-            print("Withdrawal:", amount)
-            print("-------------------------")
-
-    def show_balance(self):
-        print("Current Balance:", self.balance)
-        print("-------------------------")
-
-
-class Transaction:
-    def __init__(self, trans_id, amount):
-        self.trans_id = trans_id
-        self.amount = amount
-
-    def display_transaction(self):
-        print("Transaction ID:", self.trans_id, "Amount:", self.amount)
-        print("-----------------------------------")
-customer = Customer()
-account = Account()
+app = Flask(__name__)
+balance = 100
 transactions = []
-t_id = 0
 
-while True:
-    print("\n---------- BANK SYSTEM ------------")
-    print("1. REGISTRATION")
-    print("2. DEPOSIT")
-    print("3. WITHDRAWAL")
-    print("4. BALANCE")
-    print("5. CUSTOMER DETAILS")
-    print("6. TRANSACTION HISTORY")
-    print("7. EXIT")
+@app.route('/')
+def home():
+    return render_template('index.html', balance=balance)
 
-    choice = int(input("Enter your choice: "))
+@app.route('/deposit', methods=['POST'])
+def deposit():
+    global balance
+    amount = int(request.form['amount'])
+    balance += amount
+    transactions.append(f"Deposited: {amount}")
+    return render_template('index.html', balance=balance, msg="Deposit Successful")
 
-    if choice == 1:
-        user_id = input("Enter User Id: ")
-        if len(user_id) <= 20:
-            name = input("Enter User Name: ")
-            customer.registration(user_id, name)
-        else:
-            print("!!User id is too long!!")
+@app.route('/withdraw', methods=['POST'])
+def withdraw():
+    global balance
+    amount = int(request.form['amount'])
+    
+    if amount > balance:
+        return render_template('index.html', balance=balance, msg="Insufficient Balance")
+    
+    balance -= amount
+    transactions.append(f"Withdrawn: {amount}")
+    return render_template('index.html', balance=balance, msg="Withdrawal Successful")
 
-    elif choice == 2:
-        dep = int(input("Enter amount to deposit: "))
-        account.deposit(dep)
-        account.show_balance()
-        t_id += 1
-        transactions.append(Transaction(t_id, dep))
+@app.route('/transactions')
+def show_transactions():
+    return render_template('transactions.html', transactions=transactions)
 
-    elif choice == 3:
-        amt = int(input("Enter amount to withdraw: "))
-        account.withdrawal(amt)
-        account.show_balance()
-        t_id += 1
-        transactions.append(Transaction(t_id, amt))
-
-    elif choice == 4:
-        account.show_balance()
-
-    elif choice == 5:
-        customer.display()
-
-    elif choice == 6:
-        print("Transaction History:")
-        for t in transactions:
-            t.display_transaction()
-
-    elif choice == 7:
-        print("Thank you!")
-        break
-
-    else:
-        print("Invalid choice")
+if __name__ == '__main__':
+    app.run(debug=True)
